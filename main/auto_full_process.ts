59adfda118ee827f635365eabfca17c005edca26
dylan.yang <dylan.yang@okg.com>
2025-02-03 22:32:21 +0800

    update

--- main/auto_full_process.ts
+++ main/auto_full_process.ts
@@ -8,11 +8,6 @@ import {batchCallAbi} from "./contract";
 
 export const account = privateKeyToAccount(devnet5.accountPk)
 
- const walletClient = createWalletClient({
-    account,
-    chain: devnet5,
-    transport: http(),
-}).extend(eip7702Actions())
 
  const pubClient = createPublicClient({
     chain: devnet5,
@@ -21,44 +16,14 @@ export const account = privateKeyToAccount(devnet5.accountPk)
 
 
 async function main() {
-    const toAddress1 = '0xcb98643b8786950F0461f3B0edf99D88F274574D'
-    //0xcb98643b8786950f0461f3b0edf99d88f274574d
-    const balance1 = await getBalance(pubClient, {   address: toAddress1,  })
-    const balance1Ether = formatEther(balance1)
+    const walletClient = createWalletClient({
+        account,
+        chain: devnet5,
+        transport: http(),
+    }).extend(eip7702Actions())
 
-    console.log("balance1: " +  balance1Ether)
 
-    // const txHash = await send7702Tx(walletClient,toAddress1)
-    // console.log("tx Hash "+ txHash)
-    // const balance1After = await getBalance(pubClient, {   address: toAddress1, })
-    // const balance1AfterEther = formatEther(balance1After)
-    // const balance2After = await getBalance(pubClient, {   address: toAddress2,   })
-    // const balance2AfterEther = formatEther(balance2After)
-    // console.log("balance1After: " + balance1AfterEther)
-    // console.log("balance2After: " + balance2AfterEther)
-    //
 }
 
-async  function  info(){
-    const data =  encodeFunctionData({
-        abi: batchCallAbi,
-        functionName: 'execute',
-        args: [
-            [
-                {
-                    data: '0x',
-                    to: '0xcb98643b8786950F0461f3B0edf99D88F274574D',
-                    value: parseEther('0.001'),
-                },
-                {
-                    data: '0x',
-                    to: '0xcb98643b8786950F0461f3B0edf99D88F274574D',
-                    value: parseEther('0.001'),
-                },
-            ],
-        ]
-    });
-    console.log("data :"+  data);
-}
 
 main()
--- main/config.ts
+++ main/config.ts
@@ -12,8 +12,23 @@ export const devnet5 = /*#__PURE__*/ defineChain({
         default: {
             http: ['https://rpc.pectra-devnet-5.ethpandaops.io']},
     },
-    //0x7C0EA167B05a85c6E6ce7E919983AF3f3CEa379E
-    accountPk:'0x1e10b33439feb36a642fd48440def1dd2c5ff4c02ffbcf4b6d1c1e867fc0da6e'
+    //
+    accountPk:''
+})
+
+export const odyssey = /*#__PURE__*/ defineChain({
+    id: 911867,
+    name: 'odyssey',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Ether',
+        symbol: 'ETH',
+    },
+    rpcUrls: {
+        default: {
+            http: ['https://odyssey.ithaca.xyz']},
+    },
+    accountPk:''
 })
 export const localnode = /*#__PURE__*/ defineChain({
     id: 31337,
